a84a13c82fbbcee0953bc81b2d8a85b984158537
Mukunta <42895755+Mukunta@users.noreply.github.com>
2023-06-15 20:23:25 +0200

    new db

--- noticeboard.sql
+++ noticeboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2023 at 09:11 PM
+-- Generation Time: Jun 15, 2023 at 08:21 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -39,7 +39,8 @@ CREATE TABLE `author` (
 --
 
 INSERT INTO `author` (`id`, `name`, `email`, `password`) VALUES
-(1, 'emka', 'emkay@mail.com', '$2y$10$hWgrB4.IGb9ASJqP08tVYuKLoVBDnUiNpiHvcVctRcMqlcgb2houG');
+(3, 'master', 'master@mail.com', '$2y$10$5jrtXJH9G89nQuHggm6VfuDqMz2RgHMScgNFu7Ea94A/B7nyEWm7O'),
+(4, 'maidiebanda', 'maidie@mail.com', '$2y$10$7lATiwRfezLvl7wI81BwMOCnQSnXS3J80ROpx8zhG60HNuhGF5knK');
 
 -- --------------------------------------------------------
 
@@ -86,10 +87,11 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `content`, `category`, `author`, `cover_photo`, `date`, `important`) VALUES
-(4, 'This is the edited version', 'This is the edited version of the post.', 'staff', '1', '64566e382f246_2.jpg', '2023-05-06 15:11:52', 1),
-(5, 'jg', 'n', 'staff', '1', '64566e50a8adc_2.jpg', '2023-05-06 15:12:16', 1),
-(7, 'bene', 'swene', 'students', '1', '64567022280a2_5.jpg', '2023-05-06 15:20:02', 1),
-(8, 'bene', 'swene', 'students', '1', '6456706fdf68f_5.jpg', '2023-05-06 15:21:19', 1);
+(9, 'Subject: Annual Spring Festival - Save the Date!', 'Dear Students,\r\n\r\nWe are thrilled to announce that our annual Spring Festival will be held on Friday, April 15th, from 2:00 PM to 8:00 PM. The festival will take place in the university quad and will feature live music, food trucks, games, and various activities.\r\n\r\nThis is a fantastic opportunity to come together as a university community, enjoy the vibrant atmosphere, and create lasting memories. We encourage everyone to attend and bring your friends and family along.\r\n\r\nStay tuned for more details, including the complete schedule of performances and participating vendors. We can\'t wait to celebrate with you!\r\n\r\nBest regards,\r\nThe University Events Committee\r\n', 'students', '1', '648ac2bce576a_pexels-mikhail-nilov-9391354.jpg', '2023-06-15 07:50:20', 0),
+(10, 'Call for Research Papers - Annual University Research Symposium', 'Dear Faculty and Students,\r\n\r\nWe are pleased to announce our annual University Research Symposium, which will be held on Wednesday, May 10th. This event provides an excellent platform for researchers and scholars across various disciplines to showcase their work.\r\n\r\nWe invite faculty members and students from all departments to submit abstracts for research papers or posters. This is an invaluable opportunity to share your findings, exchange ideas, and contribute to the intellectual discourse within our university community.\r\n\r\nPlease review the submission guidelines and deadlines available on our university website. We look forward to receiving your submissions and hosting a successful Research Symposium.\r\n\r\nKind regards,\r\nThe Symposium Organizing Committee\r\n', 'students', '1', '648ac37f3b30b_dan-dimmock-3mt71MKGjQ0-unsplash.jpg', '2023-06-15 07:53:35', 1),
+(11, 'Important Safety Reminder - Campus Emergency Procedures', 'Dear Students, Faculty, and Staff,\r\n\r\nEnsuring the safety and well-being of our university community is our utmost priority. In light of recent events, we want to take this opportunity to remind everyone about the campus emergency procedures.\r\n\r\nPlease familiarize yourself with the evacuation routes, assembly points, and emergency contact numbers provided on the university website. It is crucial to be prepared and aware of the necessary actions to take in case of any emergencies.\r\n\r\nWe encourage all students, faculty, and staff to review these procedures and report any concerns or suspicious activities to the campus security office. Together, we can maintain a safe and secure learning environment.\r\n\r\nThank you for your cooperation.\r\n\r\nSincerely,\r\nThe Campus Safety Committee\r\n', 'general', '1', '648ac8a369947_pexels-monicore-134065.jpg', '2023-06-15 08:15:31', 1),
+(12, 'Internship Opportunity - Industry Partnership Program', 'Dear Students,\r\n\r\nWe are excited to announce the launch of our Industry Partnership Program, offering internship opportunities for students across various disciplines. This program aims to provide valuable hands-on experience and foster connections between our university and industry professionals.\r\n\r\nIf you are interested in gaining practical experience in your field of study and developing valuable skills, we encourage you to explore the available internship positions listed on our university website. The application process and eligibility criteria are outlined in detail on the internship portal.\r\n\r\nDon\'t miss this chance to jump-start your career and gain valuable industry exposure. Apply today and take advantage of this exciting opportunity.\r\n\r\nBest regards,\r\nThe Career Services Department', 'students', '1', '648ac8e30b1c4_pexels-anamul-rezwan-1216589.jpg', '2023-06-15 08:16:35', 1),
+(13, 'Join a Student Club - Explore Your Passions!', 'Dear Students,\r\n\r\nAre you looking to enhance your university experience, meet new people, and explore your passions? Joining a student club is a fantastic way to do just that!\r\n\r\nWe have a wide range of active student clubs covering various interests, including arts, sports, community service, academic disciplines, and more. These clubs offer opportunities for personal growth, leadership development, and the chance to make a positive impact on campus.\r\n\r\nVisit our university website to explore the list of student clubs and find contact information for each club\'s leadership. Feel free to reach out to them, attend their meetingsand events, and get involved in activities that align with your interests. Joining a student club will not only provide you with a sense of belonging but also enable you to develop new skills and create lifelong friendships.\r\n\r\nTake the first step towards an enriching university experience by joining a student club today. Don\'t miss out on the chance to make the most of your time here and create memories that will last a lifetime.\r\n\r\nBest regards,\r\nThe Student Affairs Office\r\n', 'students', '1', '648ac9357bdba_jaime-lopes-0RDBOAdnbWM-unsplash.jpg', '2023-06-15 08:17:57', 1);
 
 --
 -- Indexes for dumped tables
@@ -121,7 +123,7 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `author`
 --
 ALTER TABLE `author`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -133,7 +135,7 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
